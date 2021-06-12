21afc2b8fc17fa8d3d3a497eb39c60decc6640c7
Basti´an Olea <bastianolea@gmail.com>
2021-06-11 23:31:43 -0400

    scrapping yapo funcionando y guardado

--- scrapping/yapo.cl/scrapping_yapo.r
+++ scrapping/yapo.cl/scrapping_yapo.r
@@ -64,21 +64,23 @@ for (c in categorias) { #categorías por scrappear
     try(
       download.file(url, 
                     #nombre del archivo
-                  destfile = paste0("scrapping/yapo.cl/paginas/", 
-                                    paste("yapo",
-                                          lubridate::today(), #fecha
-                                          c, p, #categoría y página
-                                          sep = "_"),
-                                    ".html"), quiet = F), #extensión
-    silent = F)
+                    destfile = paste0("scrapping/yapo.cl/paginas/11-jun-2021/", 
+                                      paste("yapo",
+                                            lubridate::today(), #fecha
+                                            c, p, #categoría y página
+                                            sep = "_"),
+                                      ".html"), quiet = F), #extensión
+      silent = F)
     Sys.sleep(1.5) #misericordia
   }
 }
 
-
+#scrapping individual ----
 
 #cargar página descargada
 content <- read_html("scrapping/yapo.cl/temp/yapo_2021-06-10_muebles_4.html")
+content <- read_html("scrapping/yapo.cl/paginas/11-jun-2021/yapo_2021-06-11_animales_accesorios_10.html")
+
 
 
 #scrapping ----
@@ -110,9 +112,100 @@ resultado_1 <- tibble::tibble(titulo = s_titulo,
                               hora = s_hora,
                               comuna = s_comuna,
                               imagen = s_imagen)
-                                      
-resultado_1
 
-#agregar columnas del loop
 
-#agregar a lista
+#scrapping de todas las páginas ----
+fecha_s <- "11-jun-2021"
+fecha_a <- "2021-06-11"
+ruta_scrapping <- paste0("scrapping/yapo.cl/paginas/", fecha_s, "/")
+archivos_s <- list.files(ruta_scrapping)
+
+#inicializar lista
+#base_yapo <- list()
+
+archivos_s[1]
+min(archivos_s)
+
+#loop
+for(archivo in archivos_s) {
+  #iniciar loop: inicializar lista
+  if(archivo == min(archivos_s)) { 
+    cat("inicializando lista", fill=T) 
+    base_yapo_list <- list() 
+    }
+  
+  nombre_archivo <- archivo %>% stringr::str_remove(paste0("yapo_", fecha_a, "_"))
+  s_categoria <- nombre_archivo %>% stringr::str_remove(".\\d+.html")
+  s_pagina <- nombre_archivo %>% stringr::str_extract("\\d+")
+  
+  #reconsrtruir url
+  s_url <- paste0("https://www.yapo.cl/tarapaca/", s_categoria, "?ca=2_s&o=", s_pagina)
+  
+  #leer html
+  cat("iniciando ", archivo, "...", fill=T)
+  ruta_archivo <- paste0(ruta_scrapping, archivo)
+  content <- read_html(ruta_archivo)
+  
+  #obtener contenido
+  s_titulo <- content %>% html_nodes(".title") %>% html_text()
+  s_enlace <- content %>% html_nodes(".title") %>% html_attr('href') #enlaces
+  s_imagen <- content %>% html_nodes(".image") %>% html_attr('src')
+  s_fecha <- content %>% html_nodes(".date") %>% html_text() 
+  s_hora <- content %>% html_nodes(".hour") %>% html_text() 
+  s_comuna <- content %>% html_nodes(".commune") %>% html_text() 
+  
+  #obtiene los precios incluso si no hay
+  s_precio <- content %>% html_nodes(".thumbs_subject") %>%
+    #busca adentro de la class anterior, retorna NA si no encuentra
+    lapply(. %>% html_nodes(".price") %>% html_text() %>%
+             ifelse(identical(., character(0)), NA, .)) %>% 
+    unlist()
+  
+  if (length(s_titulo) < 2) { 
+    warning(paste(nombre_archivo, "sin elementos")); 
+    cat("error", fill=T)
+    next() 
+  }
+  
+  #unir columnas
+  resultado <- tibble::tibble(producto = s_titulo %>% stringr::str_trim(),
+                              precio = s_precio %>% stringr::str_remove("\\.") %>% readr::parse_number(),
+                              enlace = s_enlace,
+                              categoria = s_categoria,
+                              pagina = s_pagina,
+                              fecha = s_fecha,
+                              hora = s_hora,
+                              comuna = s_comuna,
+                              imagen = s_imagen,
+                              ruta = ruta_archivo,
+                              url = s_url,
+                              fecha_scrapping = fecha_a) %>%
+    #limpieza
+    mutate(across(c(categoria, fecha, comuna), ~ as.factor(.x))) %>%
+    mutate(fecha_scrapping = lubridate::ymd(fecha_scrapping)) %>%
+    mutate(pagina = readr::parse_number(pagina))
+  
+  base_yapo_list[[archivo]] <- resultado
+  cat("OK ", archivo, fill=T) 
+  
+  #terminar loop: transformar lista a tibble 
+  if(archivo == max(archivos_s)) { 
+    cat("convirtiendo lista...", fill=T) 
+    base_yapo <- bind_rows(base_yapo_list) 
+    }
+}
+
+#save(base_yapo, file = paste0("scrapping/yapo.cl/bases/base_yapo_", fecha_s, ".rdata"))
+#load(paste0("scrapping/yapo.cl/bases/base_yapo_", fecha_s, ".rdata"))
+
+base_yapo %>% count(categoria)
+
+base_yapo %>% count(comuna)
+
+library(ggplot2)
+
+base_yapo %>%
+  ggplot(aes(precio)) +
+  geom_histogram(bins = 90) +
+  geom_rug(alpha=0.1) +
+  scale_x_continuous(n.breaks = 10, limits = c(0, 60000))
